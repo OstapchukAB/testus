b1515d88c40c32e03f77129cd2e3e5b0292afa0b
OstapchukAB <electron.anton@gmail.com>
2025-04-14 11:22:56 +0300

    довел до ума с рекурсией

--- test_1_4.py
+++ test_1_4.py
@@ -1,25 +1,25 @@
 #current:parent
-namespace={"global":""}
-vars_in_ns={}
+namespace={"global":"-"}
+namespaces_vars={"global":[]}
 
 def create(nm_current,nm_parent):
     parent = namespace.get(nm_current)
     if parent !=nm_parent:
         namespace[nm_current]=nm_parent
+        namespaces_vars[nm_current]=[]
 
 def add(nm_current,varname):
     ns = namespace.get(nm_current)
     if ns:
-        varrs=vars_in_ns.get(nm_current)
-        if varrs:
-            if varname not in varrs:
-                vars_in_ns[nm_current]=varrs.add(varname)
+        varrs=namespaces_vars.get(nm_current)
+        if varname not in varrs:
+            namespaces_vars[nm_current].append(varname)
 def get_nm_for_var(nm,varname):
     parent=namespace.get(nm)
-    varss= vars_in_ns.get(nm) #проверим в текущем namespace
+    varss= namespaces_vars.get(nm) #проверим в текущем namespace
     if varss and varname in varss:#нашли возвращаем
         return nm
-    elif parent=="" and varname not in varss:#если в глобальном простарнстве дальше идти некуда
+    elif parent=="-" and varname not in varss:#если в глобальном простарнстве дальше идти некуда
         return None
         #поищем на верхнем уровне
     elif parent:
@@ -27,16 +27,20 @@ def get_nm_for_var(nm,varname):
 
 
 
-n=int(input())
-for _ in range(n):
-    s=input()
+#n=int(input())
+n=9
+w=["add global a","create foo global","add foo b","get foo a","get foo c","create bar foo","add bar a","get bar a","get bar b"]
+for i in range(n):
+   # s=input()
+    s=w[i]
     s=s.split()
     if s[0]=="create":       
         create(s[1],s[2])
     elif s[0]=="add":
         add(s[1],s[2])
     elif s[0]=="get":
-        get_nm_for_var(s[1],s[2])
+        result=get_nm_for_var(s[1],s[2])
+        print(result)
 
                        
                        
